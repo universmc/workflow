88a1aedfda78a0ce4d4f9b1ddfcdf1b87d6342e8
universmc <hubmaster74@gmail.com>
2024-03-18 01:15:23 +0100

    SessionStart

--- web_sat/src/inc/footer.php
+++
@@ -1,9 +0,0 @@
-<footer>
-</footer>
-<script src="src/js/addEventListener.js"></script>
-<scirpt src="src/js/srcript.js"></script>
-<!-- Scripts JavaScript de Bootstrap -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-</body>
-</html>
--- web_sat/src/inc/script.php
+++
@@ -1,2 +0,0 @@
-<script src="menu/projets/orc/P6/soutenence/src/js/groq.js"></script>
-<script src="menu/projets/orc/P6/soutenence/src/js/persona.js"></script>
